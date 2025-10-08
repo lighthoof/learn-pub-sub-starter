d1d4f8d952823dd043d53c31bea60d8499d214b8
lighthoof <andrew.kuligin@gmail.com>
2025-10-08 15:27:41 +0000

    Modified server code to work with amqp connections

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	rmqConnString := "amqp://guest:guest@localhost:5672/"
+
+	//fmt.Println("Starting Peril server...")
+	connection, err := amqp.Dial(rmqConnString)
+	if err != nil {
+		fmt.Println("Cannot obtain a server connection")
+		return
+	}
+	defer connection.Close()
+	fmt.Println("Connection successful")
+
+	sigChan := make(chan os.Signal, 2)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+	<-sigChan
+
+	fmt.Println("\nShutting down...")
 }
